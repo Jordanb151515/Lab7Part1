691c29ea3d224777a3a86b7c7e0c192c02ac70f4
Jordan Byrne <jbyrne001@student.sdccd.edu>
2025-04-22 11:00:59 -0700

    lab7part2

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 /**
- * Write a description of class ArrayMethods here.
+ * This class holds methods relating to myArray. Count, sum, average, find max and index of max,
+ * find last value, find all values, getter, copy array, print out statement, sort array, and reverse the array.
  *
  * @author Jordan Byrne
  * @version v1.0
@@ -156,6 +157,40 @@ public class ArrayMethods{
             System.out.print(inArray[i]);
         System.out.println("}");
     }
+    
+    /**
+     * This method takes the inputed array and sorts it into ascending order.
+     * 
+     * @param inArray int array to sort in ascending order
+     */
+    public void sortArray(int[] inArray)
+    {
+        for(int i=0; i<inArray.length-1; i++)
+           for(int j=i+1; j<inArray.length; j++)
+              if(inArray[j]<inArray[i]){
+                int temp = inArray[j];
+                inArray[j] = inArray[i];
+                inArray[i] = temp;
+                }
+    }
+    
+    /**
+     * Creates a new array that is the reversed version of the inputed array.
+     * 
+     * @param inArray int array to be copied and reversed
+     */
+    public int[] reverseArray(int[] inArray)
+    {
+        int[] newArray = new int[inArray.length];
+        int index = 0;
+        for(int i=inArray.length-1; i>=0; i--){
+            newArray[index] = inArray[i];
+            index++;
+        }
+        return newArray;
+    }
+    
+
 
     
 }
